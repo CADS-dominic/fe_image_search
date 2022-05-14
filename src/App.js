865a5b183ef88a9287883459edf98999a6f2dcc8
19US-dominic <quanghuy.1334@gmail.com>
2022-05-14 15:23:57 +0700

    update validation

--- src/App.js
+++ src/App.js
@@ -12,25 +12,18 @@ import {
 	Alert,
 } from '@mui/material'
 import axios from 'axios'
+import { useFormik, setFieldValue } from 'formik'
 import { useState } from 'react'
 import Dropzone from 'react-dropzone-uploader'
+import * as Yup from 'yup'
 import 'react-dropzone-uploader/dist/styles.css'
 import './App.css'
 
 function App() {
-	const [names, setNames] = useState([])
-	const [lang, setLang] = useState('')
-	const [file, setFile] = useState('')
 	const [preview, setPreview] = useState('')
 	const [isLoading, setLoading] = useState(false)
 	const [response, setResponse] = useState('')
 
-	const handleLangChange = (e) => {
-		setLang(e.target.value)
-	}
-	const handleNamesChange = (e) => {
-		setNames(e.target.value.split(','))
-	}
 	const getUploadParams = ({ meta }) => {
 		return { url: 'https://httpbin.org/post' }
 	}
@@ -41,42 +34,59 @@ function App() {
 			reader.readAsDataURL(file)
 			reader.onloadend = () => {
 				if (status === 'removed') {
-					setFile('')
 					setResponse('')
+					setPreview('')
+					formik.values.image = ''
 				} else {
-					setFile(reader.result.replace(/^data:image\/[a-z]+;base64,/, ''))
 					setPreview(reader.result)
+					formik.values.image = reader.result.replace(/^data:image\/[a-z]+;base64,/, '')
 				}
 			}
 		} else {
 			setLoading(true)
 		}
 	}
-	const handleSubmit = async (e) => {
-		e.preventDefault()
-		setLoading(true)
-		const url = 'http://118.69.218.59:7554/identify'
-		const data = new FormData()
-		data.append('names', names)
-		data.append('lang', lang)
-		data.append('image', file)
-		axios(url, {
-			method: 'POST',
-			mode: 'no-cors',
-			headers: {
-				'Content-Type': 'multipart/form-data',
-			},
-			data,
-		}).then((res) => {
-			setLoading(false)
-			setResponse(res.data)
-			console.log(res)
-		})
-	}
+	const formik = useFormik({
+		initialValues: {
+			names: '',
+			lang: '',
+			image: '',
+		},
+		validationSchema: Yup.object({
+			names: Yup.string().min(1).required(),
+			lang: Yup.string().length(2).required(),
+			image: Yup.string().min(1).required(),
+		}),
+		onSubmit: ({ names, lang, image }) => {
+			setLoading(true)
+			const url = 'http://118.69.218.59:7554/identify'
+			const data = new FormData()
+			data.append('names', names)
+			data.append('lang', lang)
+			data.append('image', image)
+			axios(url, {
+				method: 'POST',
+				mode: 'no-cors',
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+				data,
+			}).then((res) => {
+				setLoading(false)
+				setResponse(res.data)
+			})
+		},
+	})
 	return (
 		<div className='App'>
 			<Container maxWidth='sm'>
-				<form className='dropzone' onSubmit={handleSubmit}>
+				<form
+					className='dropzone'
+					onSubmit={(e) => {
+						e.preventDefault()
+						formik.handleSubmit()
+					}}
+				>
 					<Grid container spacing={2}>
 						<Grid item sm={12}>
 							<Typography variant='h2' gutterBottom component='div'>
@@ -90,17 +100,21 @@ function App() {
 								fullWidth
 								placeholder='Jean,Skirt,Tshirt,...'
 								helperText='Separate by comma, no spacebar'
-								onInput={handleNamesChange}
+								onInput={formik.handleChange}
+								value={formik.values.names}
+								name='names'
 							/>
+							{formik.errors.names ? <Alert severity='error'>{formik.errors.names}</Alert> : null}
 						</Grid>
 						<Grid item sm={12}>
 							<FormControl fullWidth>
 								<InputLabel>Language</InputLabel>
-								<Select value={lang} onChange={handleLangChange} label='Language'>
+								<Select value={formik.values.lang} name='lang' onChange={formik.handleChange} label='Language'>
 									<MenuItem value='vi'>Vietnam</MenuItem>
 									<MenuItem value='en'>English</MenuItem>
 								</Select>
 							</FormControl>
+							{formik.errors.lang ? <Alert severity='error'>{formik.errors.lang}</Alert> : null}
 						</Grid>
 						<Grid item sm={12}>
 							<Dropzone
@@ -109,22 +123,23 @@ function App() {
 								multiple={false}
 								accept='image/*'
 							/>
+							{formik.errors.image && formik.values.image === '' ? (
+								<Alert severity='error'>{formik.errors.image}</Alert>
+							) : null}
 						</Grid>
 						<Grid item sm={6}>
 							{preview && response !== '' ? <img src={preview} alt='Preview' /> : null}
 						</Grid>
 						<Grid item sm={6}>
-							{response.code === 2100 ? (
-								<Alert severity='error'>Error!</Alert>
-							) : response.code === 200 ? (
-								response.names.map((name, index) => {
-									return (
-										<Typography key={name} variant='body2' gutterBottom>
-											{name}: {response.scores[index]}
-										</Typography>
-									)
-								})
-							) : null}
+							{response.names
+								? response.names.map((name, index) => {
+										return (
+											<Typography key={name} variant='body2' gutterBottom>
+												- {name}: {response.scores[index]}
+											</Typography>
+										)
+								  })
+								: null}
 						</Grid>
 						<Grid item sm={12} textAlign='center'>
 							{isLoading ? (
